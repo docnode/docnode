deccbaf5fe653288facc93bcd7599f941bf0c998
German Jablonski <43938777+GermanJablo@users.noreply.github.com>
2025-12-02 18:02:58 +0000

    HN banner

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -3,11 +3,24 @@ import { Analytics } from "@vercel/analytics/react";
 import { Suspense } from "react";
 import PostHogPageView from "./PostHogPageView";
 import { AISearchTrigger } from "@/components/search";
+import { Banner } from "fumadocs-ui/components/banner";
 
 export default function Layout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="flex min-h-screen flex-col">
+        <Banner className="bg-emerald-700 text-white">
+          <span className="">
+            DocNode is now live! See what the community is saying on{" "}
+            <a
+              href="https://news.ycombinator.com/item?id=46124227"
+              className="underline"
+            >
+              Hacker News
+            </a>{" "}
+            🎉
+          </span>
+        </Banner>
         <Suspense fallback={null}>
           <PostHogPageView />
         </Suspense>
