7b9a7071b0bad570cb735bf593a2aca3d026e864
German Jablonski <43938777+GermanJablo@users.noreply.github.com>
2025-12-06 23:30:37 +0000

    improve test

--- tests/docnode-lexical/docnode-lexical.test.ts
+++ tests/docnode-lexical/docnode-lexical.test.ts
@@ -1,31 +1,118 @@
-import { test, expect } from "vitest";
+import { test, expect, describe } from "vitest";
 import { Doc } from "docnode";
-import { docToLexical } from "@docnode/lexical";
+import { docToLexical, LexicalDocNode } from "@docnode/lexical";
 import { assertJson } from "../docnode/utils.js";
+import { type SerializedParagraphNode, type SerializedTextNode } from "lexical";
 
-test("docnode to lexical", () => {
-  // const doc = new Doc({ extensions: [{ nodes: [LexicalDocNode] }] });
-  const { editor, doc } = docToLexical({
-    namespace: "MyEditor",
-    onError: (error) => {
-      console.error(error);
-    },
+describe("docnode to lexical", () => {
+  test("no doc provided", () => {
+    const { editor, doc } = docToLexical({
+      namespace: "MyEditor",
+      onError: (error) => {
+        console.error(error);
+      },
+    });
+    expect(doc).toBeInstanceOf(Doc);
+    const jsonEditorState = editor.getEditorState().toJSON();
+    expect(jsonEditorState).toStrictEqual({
+      root: {
+        children: [],
+        direction: null,
+        format: "",
+        indent: 0,
+        type: "root",
+        version: 1,
+      },
+    });
+    const rootJson = JSON.stringify(jsonEditorState.root);
+    expect(rootJson).toStrictEqual(
+      '{"children":[],"direction":null,"format":"","indent":0,"type":"root","version":1}',
+    );
+    assertJson(doc, ["root", {}, [["l", { j: rootJson }]]]);
   });
-  expect(doc).toBeInstanceOf(Doc);
-  const jsonEditorState = editor.getEditorState().toJSON();
-  expect(jsonEditorState).toStrictEqual({
-    root: {
+
+  test("doc provided", () => {
+    const doc = new Doc({ extensions: [{ nodes: [LexicalDocNode] }] });
+    const paragraphJson: SerializedParagraphNode = {
+      type: "paragraph",
+      version: 1,
       children: [],
-      direction: null,
       format: "",
       indent: 0,
-      type: "root",
+      direction: "ltr",
+      textFormat: 0,
+      textStyle: "",
+    };
+    const textJson: SerializedTextNode = {
+      type: "text",
       version: 1,
-    },
+      text: "Hello, world!",
+      detail: 0,
+      format: 0,
+      mode: "normal",
+      style: "",
+    };
+
+    const dnParagraph1 = doc.createNode(LexicalDocNode);
+    const dnParagraph2 = doc.createNode(LexicalDocNode);
+    const dnText1 = doc.createNode(LexicalDocNode);
+    const dnText2 = doc.createNode(LexicalDocNode);
+
+    dnParagraph1.state.j.set(paragraphJson);
+    dnParagraph2.state.j.set(paragraphJson);
+    dnText1.state.j.set(textJson);
+    dnText2.state.j.set(textJson);
+
+    dnParagraph1.append(dnText1);
+    dnParagraph2.append(dnText2);
+    doc.root.append(dnParagraph1, dnParagraph2);
+
+    assertJson(doc, [
+      "root",
+      {},
+      [
+        [
+          "l",
+          { j: JSON.stringify(paragraphJson) },
+          [["l", { j: JSON.stringify(textJson) }]],
+        ],
+        [
+          "l",
+          { j: JSON.stringify(paragraphJson) },
+          [["l", { j: JSON.stringify(textJson) }]],
+        ],
+      ],
+    ]);
+
+    const { editor } = docToLexical(
+      {
+        namespace: "MyEditor",
+        onError: (error) => {
+          console.error(error);
+        },
+      },
+      doc,
+    );
+    expect(doc).toBeInstanceOf(Doc);
+    const jsonEditorState = editor.getEditorState().toJSON();
+    expect(jsonEditorState).toStrictEqual({
+      root: {
+        children: [
+          {
+            ...paragraphJson,
+            children: [textJson],
+          },
+          {
+            ...paragraphJson,
+            children: [textJson],
+          },
+        ],
+        direction: null,
+        format: "",
+        indent: 0,
+        type: "root",
+        version: 1,
+      },
+    });
   });
-  const rootJson = JSON.stringify(jsonEditorState.root);
-  expect(rootJson).toStrictEqual(
-    '{"children":[],"direction":null,"format":"","indent":0,"type":"root","version":1}',
-  );
-  assertJson(doc, ["root", {}, [["l", { j: rootJson }]]]);
 });
